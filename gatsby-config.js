3e7198afe32943549260ef6eaf190137a3c42cf6
super-marios <wjckorea001@gmail.com>
2020-09-21 00:31:27 +0900

    changed profile-pic

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Mario Choi`,
+      summary: `This is my first blog with Gatsby.js.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
